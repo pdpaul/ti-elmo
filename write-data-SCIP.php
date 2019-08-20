95e684e0afa2a2d9302eb2bfeea9294d0bfdddb3
Pooja Paul <poojapaul149@gmail.com>
2019-08-20 11:17:56 -0700

    further modifications to saveData fn and php code

--- write-data-SCIP.php
+++ write-data-SCIP.php
@@ -2,8 +2,9 @@
 
 <?php
 $post_data = json_decode(file_get_contents('php://input'), true); 
+$time = date("Y-m-d-H-i");
 // the directory "data" must be writable by the server
-$name = "data/".$post_data['filename'].".csv"; //ALT: "SCIP_PP/data/"
+$name = "data/".$post_data['filename'].$time.".csv"; //adding timestamp to file name
 $data = $post_data['filedata'];
 // write the file to disk
 file_put_contents($name, $data);
